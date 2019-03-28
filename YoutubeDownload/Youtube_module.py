573096650e5b8643f750c1d33d5e47b19c8305ce
jiseagle <eagle.norman6555@gmail.com>
2019-03-28 19:44:48 +0800

    add audio only download, and correct filename issue.

--- YoutubeDownload/Youtube_module.py
+++ YoutubeDownload/Youtube_module.py
@@ -9,6 +9,7 @@ from pytube import YouTube
 import threading
 import tkinter as tk
 import os
+import subprocess
 
 lock = threading.Lock()
 
@@ -28,12 +29,52 @@ def start_download(url, listBox):
         os.mkdir('download')
         yt.streams.first().download(os.getcwd()+os.sep+'download')  #開始下載影片
     
-    
     lock.acquire()
     listBox.delete(num)
     listBox.insert(num, f'下載完成: {num:02d}:{title}')
     lock.release()
+
+def start_downloadm(url, listBox):
+    yt = YouTube(url)
+    title = yt.title           #影片名稱
     
+    name = str()               # remove '.', '/' from file name.
+    for i in title:
+        if i == '.':
+            name = name
+        elif i == '/':
+            name = name
+        elif i ==':':
+            name=name
+        else:
+            name = name + i
+    
+    name1  = name + ".mp4"
+    name2 = name + ".mp3"
+    
+    lock.acquire()
+    num = listBox.size()
+    listBox.insert(tk.END, f'下載中: {num:02d}:{title}')
+    lock.release()
+    
+    if os.path.exists(os.getcwd()+os.sep+'download'):
+        yt.streams.first().download(output_path=os.getcwd()+os.sep+'download', filename=title)  #開始下載影片
+    
+    else:
+        os.mkdir('download')
+        yt.streams.first().download(output_path=os.getcwd()+os.sep+'download', filename=title)  #開始下載影片
+    
+    # Use ffmpeg transfer mp4 to mp3   
+    subprocess.call(['ffmpeg', '-i', os.path.join(os.getcwd()+os.sep+'download', name1),
+                    os.path.join(os.getcwd()+os.sep+'download', name2)])
+    
+    os.remove(os.getcwd()+os.sep+'download'+os.sep+name1)  #remove mp4 file
+    
+    lock.acquire()
+
+    listBox.delete(num)
+    listBox.insert(num, f'下載完成: {num:02d}:{title}')
+    lock.release()
 
 def get_urls(url):
     urls = []
@@ -59,13 +100,13 @@ def get_urls(url):
         if ('&index=' in url1) and (url1 not in urls):
             urls.append(url1)
     
-    # ----- 取得各個影片的title ----------- 
+    """# ----- 取得各個影片的title ----------- 
     title_list = bs.find_all('h4', {'class':'yt-ui-ellipsis yt-ui-ellipsis-2'})
     
     for i in title_list:
         title = i.get_text().strip()
         if title not in title1:
-            title1.append(title)
+            title1.append(title)"""
             
-    return urls, title1
+    return urls
        
